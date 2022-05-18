1a524afd2ad2a2ac9167da9c0fef3a7dc2ee2a0f
Trevor <trevor.bos@outlook.com>
2022-05-18 19:01:44 -0400

    Fixed issue where the program broke after multiple clicks with help from TA Anders

--- script.js
+++ script.js
@@ -9,75 +9,77 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var specialCharacters = "!@#$%^&*(){}<>?:+_-/\\||~";
-var userCharacters = "";
+
+console.log (specialCharacters);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 //function for generating the password, the function will then be called in the writePassword() function returning the final password.
-function generatePassword(){
+function generatePassword() {
+  var userCharacters = "";
   var userPassword = "";
 
-  var characterLength = prompt("How long would you like your password to be? (Between 8 and 128)");
-//The isNaN() function is used here to tell whether the given number is illegal or not. Googled best way to check this.
-  if(isNaN(characterLength)){
+  var characterLength = prompt(
+    "How long would you like your password to be? (Between 8 and 128)"
+  );
+  //The isNaN() function is used here to tell whether the given number is illegal or not. Googled best way to check this.
+  if (isNaN(characterLength)) {
     alert("You must input a number!");
-    return generatePassword()
+    return generatePassword();
   }
   if (characterLength < 8 || characterLength > 128) {
-    alert ("Please choose numbers between 8 and 128!")
-    return generatePassword()
+    alert("Please choose numbers between 8 and 128!");
+    return generatePassword();
   }
 
-//Display prompts asking the user to choose what specifics they would like their password to have
-  var confirmUpper = confirm("Would you like your password to have UPPER case letters?");
-  var confirmLower = confirm("Would you like your password to have LOWER case letters?");
-  var confirmNumbers = confirm("Would you like your password to have NUMBERS in it?");
-  var confirmSpecial = confirm("Would you like your password to have SPECIAL characters in it?");
+  //Display prompts asking the user to choose what specifics they would like their password to have
+  var confirmUpper = confirm(
+    "Would you like your password to have UPPER case letters?"
+  );
+  var confirmLower = confirm(
+    "Would you like your password to have LOWER case letters?"
+  );
+  var confirmNumbers = confirm(
+    "Would you like your password to have NUMBERS in it?"
+  );
+  var confirmSpecial = confirm(
+    "Would you like your password to have SPECIAL characters in it?"
+  );
 
   //adding the "!" infront of these variables checks to see if they are NOT true.
-  if (!confirmUpper && !confirmLower && !confirmNumbers && !confirmSpecial){
+  if (!confirmUpper && !confirmLower && !confirmNumbers && !confirmSpecial) {
     alert("Please choose at least one character type!");
-    return generatePassword()
+    return generatePassword();
   }
   //Checks to see if they are true.
-  if (confirmUpper){
-    userCharacters += upperCase
+  if (confirmUpper) {
+    userCharacters += upperCase;
   }
-  if (confirmLower){
-    userCharacters += lowerCase
+  if (confirmLower) {
+    userCharacters += lowerCase;
   }
   if (confirmNumbers) {
-    userCharacters += numbers
+    userCharacters += numbers;
   }
   if (confirmSpecial) {
-    userCharacters += specialCharacters
+    userCharacters += specialCharacters;
   }
   //This chooses random letters, numbers, special characters, and creates the password with the given length that the user has inputted.
-  for (var index = 0; index < characterLength; index++){
-  //charAt is a function that returns the character from the chosen index, also looked up on google to make the best use.
-    userPassword += userCharacters.charAt(Math.floor(Math.random() * userCharacters.length));
+  for (var index = 0; index < characterLength; index++) {
+    //charAt is a function that returns the character from the chosen index, also looked up on google to make the best use.
+    userPassword += userCharacters.charAt(
+      Math.floor(Math.random() * userCharacters.length)
+    );
   }
-  return  userPassword;
-
-  var confirmPassword = confirm ("Do you like this password?");
+  return userPassword;
 }
 
-//function for upperCase generation
-
-//function for lowerCase generation
-
-//function for number generation
-
-//function for specialCharacter generation
-
-//Add conditions and alerts if user has chosen invalid options
