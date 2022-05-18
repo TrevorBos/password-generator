292d86481ee4b5a2b7f1c58e105e7bb97ec2721f
Trevor <trevor.bos@outlook.com>
2022-05-18 13:35:21 -0400

    Made the button work, prompts the user to choose what kind of specifics they would like their password to have, and returns the chosen password into the box.

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,7 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var specialCharacters = "!@#$%^&*(){}<>?:+_-/\\||~";
-var chosenCharacters = "";
+var userCharacters = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -31,11 +31,11 @@ function generatePassword(){
 //The isNaN() function is used here to tell whether the given number is illegal or not. Googled best way to check this.
   if(isNaN(characterLength)){
     alert("You must input a number!");
-    return generatePassword();
+    return generatePassword()
   }
   if (characterLength < 8 || characterLength > 128) {
     alert ("Please choose numbers between 8 and 128!")
-    return generatePassword();
+    return generatePassword()
   }
 
 //Display prompts asking the user to choose what specifics they would like their password to have
@@ -43,6 +43,29 @@ function generatePassword(){
   var confirmLower = confirm("Would you like your password to have LOWER case letters?");
   var confirmNumbers = confirm("Would you like your password to have NUMBERS in it?");
   var confirmSpecial = confirm("Would you like your password to have SPECIAL characters in it?");
+
+  if (!confirmUpper && !confirmLower && !confirmNumbers && !confirmSpecial){
+    alert("Please choose at least one character type!");
+    return generatePassword()
+  }
+  if (confirmUpper){
+    userCharacters += upperCase
+  }
+  if (confirmLower){
+    userCharacters += lowerCase
+  }
+  if (confirmNumbers) {
+    userCharacters += numbers
+  }
+  if (confirmSpecial) {
+    userCharacters += specialCharacters
+  }
+
+  for (var index = 0; index < characterLength; index++){
+  //charAt is a function that returns the character from the chosen index.
+    userPassword += userCharacters.charAt(Math.floor(Math.random() * userCharacters.length));
+  }
+  return  userPassword;
 }
 
 //function for upperCase generation
